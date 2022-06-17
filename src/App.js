24e3abfeca0e502f2d84016756b7133f59a10827
Austin Gae <austingae@gmail.com>
2022-06-17 16:07:59 -0700

    CreatedHeader.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import HeaderFunction from './Components/Header';
+
+function App() {
+  return (
+    <div className="Body">
+      <HeaderFunction />
+    </div>
+  );
+}
+
+export default App;
+
+/*
+Task #1: Create a Header.js, and then create a <header>
+*/
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderFunction = () => {
+  return (
+    <header>
+      <div className ="HeaderContainer">
+        <h1 className="TaskTrackerTitle">Task Tracker</h1>
+        <button className ="AddButton">Add</button>
+      </div>
+    </header>
+  )
+}
+
+export default HeaderFunction
