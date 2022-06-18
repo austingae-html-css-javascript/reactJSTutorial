882dd4522d59850b66b84116702bae7326dcaa9f
Austin Gae <austingae@gmail.com>
2022-06-18 13:39:31 -0700

    I learned what ReactJS components, props, and useState are. Also, I have begun to familiarize myself with ReactJS.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-import HeaderFunction from './Components/Header';
+import { useState } from "react";
+
+
 
 function App() {
+
+  const [color, setColor] = useState("orange");
+  const [count, setCount] = useState(0);
+
+  let changeColor = () => {
+    /*setColor() updates "color" from "orange" to "document.getElementById("inputBox").value"*/
+    setColor(document.getElementById("inputBox").value); 
+  }
+
+  let findColor = () => {
+    console.log(color);
+  }
+
+  let decreaseByOne = () => {
+    /*setCount() decreases "count" from "count = 0" to "count = prevCount - 1" */
+    setCount((prevCount) => prevCount - 1)
+  }
+
+  let increaseByOne = () => {
+    /*setCount() increases "count" from "count = 0" to "count = prevCount + 1"*/
+    setCount((prevCount) => prevCount + 1)
+  }
+
+
+
+
   return (
-    <div className="Body">
-      <HeaderFunction />
+    <div className="Body" style={{textAlign: "center"}}>
+      <input id="inputBox" type="text"></input>
+      <button onClick={changeColor}>Change Color</button>
+      <p>{color}</p>
+      <button onClick={findColor}></button>
+
+
+
+      <button onClick={decreaseByOne}>-</button>
+      <p>{count}</p>
+      <button onClick={increaseByOne}>+</button>
     </div>
   );
 }
@@ -13,5 +49,6 @@ function App() {
 export default App;
 
 /*
-Task #1: Create a Header.js, and then create a <header>
+Task #1: Using useState(), when a button is clicked, update
+variable value from "orange" to "blue"
 */
