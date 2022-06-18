c72ec311e8bf5881188638d3037099e8ce8a09ec
Austin Gae <austingae@gmail.com>
2022-06-18 13:57:53 -0700

    Set up ReactJS project

--- src/App.js
+++ src/App.js
@@ -1,54 +1,9 @@
-import './App.css';
-import { useState } from "react";
+import React from 'react'
 
-
-
-function App() {
-
-  const [color, setColor] = useState("orange");
-  const [count, setCount] = useState(0);
-
-  let changeColor = () => {
-    /*setColor() updates "color" from "orange" to "document.getElementById("inputBox").value"*/
-    setColor(document.getElementById("inputBox").value); 
-  }
-
-  let findColor = () => {
-    console.log(color);
-  }
-
-  let decreaseByOne = () => {
-    /*setCount() decreases "count" from "count = 0" to "count = prevCount - 1" */
-    setCount((prevCount) => prevCount - 1)
-  }
-
-  let increaseByOne = () => {
-    /*setCount() increases "count" from "count = 0" to "count = prevCount + 1"*/
-    setCount((prevCount) => prevCount + 1)
-  }
-
-
-
-
+const App = () => {
   return (
-    <div className="Body" style={{textAlign: "center"}}>
-      <input id="inputBox" type="text"></input>
-      <button onClick={changeColor}>Change Color</button>
-      <p>{color}</p>
-      <button onClick={findColor}></button>
-
-
-
-      <button onClick={decreaseByOne}>-</button>
-      <p>{count}</p>
-      <button onClick={increaseByOne}>+</button>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
-
-/*
-Task #1: Using useState(), when a button is clicked, update
-variable value from "orange" to "blue"
-*/
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +10,32 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+Step 1) Index.js
+  1) Create Index.js
+  2) Type: 
+    import React from "react";
+    import ReactDOM from 'react-dom/client';
+    import App from "./App";
+  3) Type: 
+      const root = ReactDOM.createRoot(document.getElementById('root'));
+
+      root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+Step 2) App.js
+  1) Create App.js
+  2) Type: 
+    import React from 'react'
+
+    const App = () => {
+      return (
+        <div>App</div>
+      )
+    }
+    export default App
+
+Result: Index.js will make <div id="root"></div> in Index.html display <App /> in App.js
+*/
